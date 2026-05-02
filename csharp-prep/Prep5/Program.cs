02a8d060bc7e1a253feee3d41b39e72829b98a70
William Bria <willlax04@gmail.com>
2026-05-02 16:50:32 -0600

    Prep5 complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string uName = AskName();
+        int uNumber = AskNumber();
+        int squaredNumber = SquareNumber(uNumber);
+        int birthYear;
+        AskBirthYear(out birthYear);
+        DisplayResult(uName, squaredNumber, birthYear);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the system.");
+    }
+
+    static string AskName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int AskNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    
+    static void AskBirthYear(out int birthYear)
+    {
+        Console.Write($"Please enter the year you were born: ");
+        birthYear = int.Parse(Console.ReadLine());
+
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+        Console.WriteLine($"{name}, you will turn {2025 - birthYear} years old this year.");
     }
 }
