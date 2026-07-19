1868fcc8176cc4d71de394220fdc132cea23d405
William Bria <willlax04@gmail.com>
2026-07-18 22:15:18 -0600

    Completed final project

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        Console.Title = "Mech Combat Simulator";
+
+        Game game = new Game();
+        game.StartGame();
+
+        Console.WriteLine();
+        Console.WriteLine("Thanks for playing!");
+        Console.WriteLine("Press Enter to exit.");
+        Console.ReadLine();
     }
 }
